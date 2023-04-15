8527550eb2383087417598db6233c3ae80edb241
Hemanthsaikumarbethapudi <121763290+Hemanthsaikumarbethapudi@users.noreply.github.com>
2023-04-15 19:04:02 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+
+    $firstName = $_POST['firstname'];
+    $lasttName = $_POST['lastname'];
+    $Gender = $_POST['gender'];
+    $PhoneNumber = $_POST['phone-number'];
+    $Email = $_POST['email'];
+    $Message = $_POST['message'];
+
+
+
+
+
+?>
